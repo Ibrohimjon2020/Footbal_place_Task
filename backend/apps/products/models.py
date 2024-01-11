269e89d62dcbced8f151da6ddc798490cd869f48
unknown <asilbekdavid@gmail.com>
2024-01-11 17:18:23 +0500

    add time

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=25)
     description = models.TextField(max_length=500)
-    price = models.DecimalField(max_digits=10,decimal_places=3)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    prepare_time = models.PositiveIntegerField(blank=True)
+    prepare_time = models.PositiveIntegerField(blank=True, default=0)
     category = models.ForeignKey(to='categories.Category', on_delete=models.CASCADE)
     
     def __str__(self):

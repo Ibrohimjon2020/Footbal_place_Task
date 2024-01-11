be9483cd78a1d2944cb0d68df4a2e0fb44d01b1f
unknown <asilbekdavid@gmail.com>
2024-01-11 12:11:31 +0500

    fixed some

--- backend/apps/products/admin.py
+++ backend/apps/products/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import ProductGallery,Products
+from .models import ProductGallery,Product
 
 
 # Register your models here.
 
-admin.site.register(Products)
+admin.site.register(Product)
 admin.site.register(ProductGallery)
 
--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -1,20 +1,25 @@
 from django.db import models
-from apps.categories.models import Categorys
+from apps.categories.models import Category
 # Create your models here.
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=25)
     description = models.TextField(max_length=500)
     price = models.DecimalField(max_digits=10,decimal_places=3)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(Categorys, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
     
     
 
 
 class ProductGallery(models.Model):
     media = models.FileField(upload_to='media')
-    product = models.ForeignKey('Products', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.product.name

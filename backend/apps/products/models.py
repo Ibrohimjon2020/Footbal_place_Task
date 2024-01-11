2089c96000529f031fcb54c39ff5f17739b29b15
unknown <asilbekdavid@gmail.com>
2024-01-11 13:24:43 +0500

    add to

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from apps.categories.models import Category
 # Create your models here.
 
 class Product(models.Model):
@@ -8,7 +7,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places=3)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(to='categories.Category', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name

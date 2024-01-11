85c2f44850586cd77ed3ac4f6b10345065552a10
unknown <asilbekdavid@gmail.com>
2024-01-11 17:13:42 +0500

    add time filed

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places=3)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    prepare_time = models.PositiveIntegerField(blank=True)
     category = models.ForeignKey(to='categories.Category', on_delete=models.CASCADE)
     
     def __str__(self):

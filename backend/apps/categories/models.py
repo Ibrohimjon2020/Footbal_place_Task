888e4d51f337cf6a0af2e5691ba639834da3c40c
Ibrohimjon2020 <maxamadraximovibrohimjon@gmail.com>
2024-01-15 16:58:56 +0500

    modify models add sub_id to Category models and seriallizers

--- backend/apps/categories/models.py
+++ backend/apps/categories/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=25)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     

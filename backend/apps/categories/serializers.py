714d97f2ce81ae073c45f413c2c9256da84130f3
Ibrohimjon2020 <maxamadraximovibrohimjon@gmail.com>
2024-01-16 15:49:17 +0500

    add filds hashtag_name and head_name to Category app

--- backend/apps/categories/serializers.py
+++ backend/apps/categories/serializers.py
@@ -22,7 +22,7 @@ class CategorySubSerializer(serializers.ModelSerializer):
     products = serializers.SerializerMethodField()
     class Meta:
         model = Category
-        fields = ["id", "name", "created", "updated", "products"]
+        fields = ["id", "name", "description", "title", "image", "head_name", "hashtag_name", "created", "updated", "products"]
     
     def get_products(self, obj):
         products = Product.objects.filter(category=obj)[
@@ -36,10 +36,17 @@ class CategorySerializer(serializers.ModelSerializer):
     parent = CategorySubSerializer(required=False)
     class Meta:
         model = Category
-        fields = ["id", "name", "parent", "created", "updated", "products"]
+        fields = ["id", "name", "description", "title", "image", "head_name", "hashtag_name", "parent", "created", "updated", "products"]
 
     def get_products(self, obj):
         products = Product.objects.filter(category=obj)[
             :5
         ]  # Har bir kategoriya uchun 5 mahsulot
         return ProductSerializer(products, many=True).data
+
+class CategoryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name", "description", "title", "image", "head_name", "hashtag_name", "parent", "created", "updated"]
+
+    

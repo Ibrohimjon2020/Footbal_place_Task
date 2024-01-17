5f872aa70d2f424ddc8b76b10ab785edf5e54271
Asilbek-MR <asilbekdavid@gmail.com>
2024-01-17 11:55:16 +0500

    fixed title legth

--- backend/apps/categories/models.py
+++ backend/apps/categories/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     name = models.CharField(max_length=25)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     description = models.TextField(max_length=1000, blank=True)
-    title = models.CharField(max_length=25, null=True, blank=True)
+    title = models.CharField(max_length=100, null=True, blank=True)
     image = models.ImageField(upload_to='category_images', blank=True)
     head_name = models.CharField(max_length=25, null=True, blank=True)
     hashtag_name = models.CharField(max_length=25, null=True, blank=True)

cf6913423eb32601c44554d562fbd31040304ea5
Boburshoh-oss <Boburbekbotirov868@gmail.com>
2024-01-19 15:10:03 +0500

    fix image url

--- backend/apps/products/serializers.py
+++ backend/apps/products/serializers.py
@@ -2,17 +2,23 @@ from rest_framework import serializers
 from .models import Product, Banner
 from django.conf import settings
 
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+
     def to_representation(self, instance):
-            representation = super().to_representation(instance)
+        representation = super().to_representation(instance)
+
+        if instance.image and hasattr(instance.image, "url"):
             domain_name = settings.DOMAIN_NAME
             full_path = domain_name + instance.image.url
-            representation['image'] = full_path
-            return representation
-        
+            representation["image"] = full_path
+        else:
+            representation["image"] = None
+
+        return representation
 
 
 class BannerSerializer(serializers.ModelSerializer):

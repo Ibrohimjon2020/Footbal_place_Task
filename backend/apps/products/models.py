370cbc516348440afcb7d851d4483b88d17e7838
Asilbek-MR <asilbekdavid@gmail.com>
2024-01-23 12:08:32 +0500

    add null=true

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -22,7 +22,7 @@ class Banner(models.Model):
     title = models.CharField(max_length=250)
     summary = models.CharField(max_length=255)
     image = models.ImageField(upload_to='images/banner')
-    video = models.FileField(upload_to='banners/video')
+    video = models.FileField(upload_to='banners/video', null=True)
     body = models.TextField()
     is_active = models.BooleanField(default=True)
     

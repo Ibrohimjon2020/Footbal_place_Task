da341fe10c9a05025ed3af8a6772ec24f89732df
Asilbek-MR <asilbekdavid@gmail.com>
2024-01-24 10:51:20 +0500

    add gif url

--- backend/apps/products/serializers.py
+++ backend/apps/products/serializers.py
@@ -22,6 +22,25 @@ class ProductSerializer(serializers.ModelSerializer):
         return representation
 
 
+class ProductSerializerForUrl(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id","gif"]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        if instance.gif and hasattr(instance.gif, "url"):
+            domain_name = settings.DOMAIN_NAME
+            full_path = domain_name + instance.gif.url
+            representation["gif"] = full_path
+        else:
+            representation["gif"] = None
+
+        return representation
+
+
+
 class BannerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Banner
